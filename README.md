2b7970da727cd41873c00c4bba04a14b675fb480
Minseok-Choi <cms05041@gmail.com>
2022-03-22 17:02:52 +0900

    feat : test ing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # java-was
 Java Web Server Project for CodeSquad Members 2022
+
+1. 작동방식 정리
+2. I/O stream 관련 공부(데이터,프린트라이터)
+3. 
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,8 +4,12 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
 
 import com.google.common.base.Strings;
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
@@ -32,6 +36,9 @@ public class RequestHandler extends Thread {
                 headerLine = bufferedReader.readLine();
             }
             String url = HttpRequestUtils.parseUrl(requestLine);
+            // /user/create?userId="tany"&password="1234"
+
+            url = testMethod(url);
 
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -46,6 +53,24 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String testMethod(String requestLine) {
+        if(requestLine.contains("?")) {
+            return requestLine;
+        }
+        return signUp(requestLine);
+    }
+
+    private String signUp(String requestLine) {
+        String[] split = requestLine.split("\\?");
+        // 회원가입로직
+        String query = split[1];
+        Map<String, String> map = HttpRequestUtils.parseQueryString(query);
+        User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+        DataBase.addUser(user);
+
+        return "/index.html";
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
