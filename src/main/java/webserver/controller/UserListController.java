463faf8a44449af4778d4dc4838cce95149a812c
ByeoungJoon Jeon <juni8453@naver.com>
2022-04-01 16:16:35 +0900

    feat : UserListController 객체 생성
    유저 목록 페이지 처리를 위한 객체를 생성함

---
+++ src/main/java/webserver/controller/UserListController.java
@@ -0,0 +1,64 @@
+package webserver.controller;
+
+import db.DataBase;
+import model.User;
+import webserver.request.Request;
+import webserver.response.Response;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+public class UserListController implements BackController {
+
+    private static final String STATUS200 = "200 Ok ";
+
+    @Override
+    public Response process(Request request) {
+        Response response = new Response(request.getProtocol(), STATUS200);
+        return showUser(response);
+    }
+
+    private Response showUser(Response response) {
+        Collection<User> users = DataBase.findAll();
+
+        response.saveBody("/user/list.html");
+
+        byte[] body = response.getBody().orElseThrow();
+        String bodyLine = new String(body, StandardCharsets.UTF_8);
+
+        String[] split = bodyLine.split("\n");
+        StringBuffer stringBuffer = new StringBuffer();
+
+        for (String s : split) {
+            if (s.contains("{{$model}}")) {
+                stringBuffer
+                        .append("<tBody>").append("\n")
+                        .append("<tr>").append("\n");
+                int userIdx = 0;
+                for (User user : users) {
+                    userIdx++;
+                    String userId = user.getUserId();
+                    String name = user.getName();
+                    String email = user.getEmail();
+
+                    stringBuffer
+                            .append("<th scope=\"row\">").append(userIdx).append("</th>").append("\n")
+                            .append("<td>").append(userId).append("</td>").append("\n")
+                            .append("<td>").append(name).append("</td>").append("\n")
+                            .append("<td>").append(email).append("</td>").append("\n")
+                            .append("<td><a href=\"#\" class=\"btn btn-success\" role=\"button\">수정</a></td>").append("\n")
+                            .append("</tr>").append("\n");
+                }
+                stringBuffer
+                        .append("</tBody>")
+                        .append("\n");
+                continue;
+            }
+
+            stringBuffer.append(s).append("\n");
+        }
+
+        response.changeBodyWithModel(stringBuffer.toString().getBytes(StandardCharsets.UTF_8));
+
+        return response;
+    }
+}
